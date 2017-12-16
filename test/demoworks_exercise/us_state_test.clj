2ee435b901abf11fffa46307fa92be8418621516
Anonymous Applicant <anonymous@example.com>
2017-12-16 13:12:16 -0800

    Add a todo

--- test/demoworks_exercise/us_state_test.clj
+++ test/demoworks_exercise/us_state_test.clj
@@ -7,5 +7,6 @@
     (is (= 61
            (count postal-abbreviations))))
   (testing "some of the best states and districts are present"
+    ;; TODO: Add my own favorite state to this ;D (Skipping for now to maintain anonymity.)
     (is (every? (set postal-abbreviations)
                 #{"CO" "NY" "CA" "KS" "DC" "IL" "WA"}))))
