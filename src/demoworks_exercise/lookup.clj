01f6d35e05703b6b4a98651dcbd21bf77b75daf9
Anonymous Applicant <anonymous@example.com>
2017-12-16 14:03:30 -0800

    OCD rendering starting to come together; tests pass
    Currently, can parse into one of a variety of formats! Ooo ahh.

--- src/demoworks_exercise/lookup.clj
+++ src/demoworks_exercise/lookup.clj
@@ -1,5 +1,6 @@
 (ns demoworks-exercise.lookup
-  (:require [clj-http.client :as http]))
+  (:require [clj-http.client :as http]
+            [clojure.string :as str]))
 
 ;; Like-a dis:
 ;; https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:al,ocd-division/country:us/state:al/place:birmingham'
@@ -10,6 +11,43 @@
 ;; TODO: always nice to have a more flexible way of putting together endpoints,
 ;; but this is fast.
 (def query-endpoint "elections/upcoming")
+
+(def ocd-division "ocd-division")
+(def ocd-country "country:us")
+(def ocd-base [ocd-division ocd-country])
+
+;; Control structures; lets me only build OCD strings with keys I can presently
+;; support.
+;; The federal case is only the ocd-country string; hrm.
+(def state-keys    [:state])
+(def city-keys     [:state :place])
+(def county-keys   [:state :county])
+(def known-formats [state-keys city-keys county-keys])
+
+;;------------------------------------Data--------------------------------------
+;; Who doesn't love a consistently-shaped request?
+;; TODO: Remember to render BOTH state and "more precise" (e.g. place) OCDs
+
+(defrecord OCDEntry [street place state zip county district])
+
+;; Do I want this?
+;; {:keys [street city state zip country district]}
+(defn render-ocd
+  "Render a single OCD to a correctly formatted lookup string"
+  [ocd-entry key-order]
+  (let [pieces (into ocd-base
+                     (for [k key-order
+                           :let [ocd-val (k ocd-entry)
+                                 k-name   (name k)]]
+                       (str k-name ":" ocd-val)))]
+    (str/join "/" pieces)))
+
+(defn render-ocds
+  "Render the list of OCDs into a correctly formatted query string"
+  [ocd-entry]
+  (let [ocd-strings (for [ks known-formats]
+                      (render-ocd ocd-entry ks))]
+    (str/join "," ocd-strings)))
 
 ;;----------------------------------Requests------------------------------------
 
--- test/demoworks_exercise/lookup_test.clj
+++ test/demoworks_exercise/lookup_test.clj
@@ -1,3 +1,16 @@
 (ns demoworks-exercise.lookup-test
   (:require [demoworks-exercise.lookup :as sut]
-            [clojure.test :as t]))
+            [clojure.test :refer :all]))
+
+(def good-entry (sut/->OCDEntry "123 Place St" "oakland" "ca" "94606" "alameda" nil))
+
+(deftest data-rendering-tests
+  (testing "Can we correctly format `OCDEntry`s?"
+    (let [expected-country "ocd-division/country:us"
+          expected-state   (str expected-country "/state:ca")
+          expected-city    (str expected-state "/place:oakland")
+          expected-county  (str expected-state "/county:alameda")]
+      (is (= expected-country (sut/render-ocd good-entry [])) "Should get back our country string")
+      (is (= expected-state   (sut/render-ocd good-entry sut/state-keys)) "Should get back our state string")
+      (is (= expected-city    (sut/render-ocd good-entry sut/city-keys)) "Should get back our city string")
+      (is (= expected-county  (sut/render-ocd good-entry sut/county-keys))) "Should get back our county string")))
